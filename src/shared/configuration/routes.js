22ec38689ade5d94de7801d19a1c28afd584f166
Ricse Anthony <anthony.ricse@unmsm.edu.pe>
2024-09-27 12:48:29 -0500

    initial commit

--- src/shared/configuration/routes.js
+++
@@ -1,8 +0,0 @@
-export const Routes = {
-  Login: "/auth",
-  Home: "Inicio",
-  Finance: "/finance",
-  Products: "/products",
-  Inventory: "/inventory",
-  Scanner: "/Scanner",
-};
--- src/shared/utils/constant.js
+++
@@ -1,7 +0,0 @@
-export const COLORS = {
-  white: "#F9F9F9",
-  black: "#222222",
-  primaryOrange: "rgb(249, 112, 0)",
-  secondaryOrange: "#FFE1B6",
-  grey: "#CCCCCC",
-};
